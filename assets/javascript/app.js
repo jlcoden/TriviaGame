42778af8117cdd4e4b9a0a9e1a76585ee07c2e6a
Josh Cosson <joshlcosson@gmail.com>
2019-09-22 01:44:29 -0700

    Added background and styled index.html page. Began work on displayQuestion function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,52 +7,69 @@
 let counter = 30;
 var correctAnswers = 0; 
 var incorrectAnswers = 0; 
-var unanswered = 0; 
-
+var unanswered = 0;
+const currentQuestion = 0;  
 
 var questions = [
 
-{
-    question:"This George R.R Martin series of books was adapted into HBO's popular 'Game of Thrones' Tv series",
-    choices: ["The Chronicals of Narnia", "A song of Ice and fire", "The Rain Wilds Chronicals", "Iron Swords Trilogy"],
-    correctChoice: "A song of Ice and fire"
+    {
+        question:"This George R.R Martin series of books was adapted into HBO's popular 'Game of Thrones' Tv series",
+        choices: ["The Chronicals of Narnia", "A song of Ice and fire", "The Rain Wilds Chronicals", "Iron Swords Trilogy"],
+        correctChoice: "A song of Ice and fire"
+    
+    },
+    
+    {
+        question: "In what book do 24 children fight to the death?" ,
+        choices: ["Harry Potter", "Hunger Games", "Eragon", "Lord of the Rings"],
+        correctChoice: "Hunger Games"
+    },
+    
+    { 
+        question: "What is the name of the lion resurrected in C.S. Lewis' tales of Narnia?",
+        choices: ["Adam" , "Jesus", "Aslan", "James"],
+        correctChoice: "Aslan"
+    
+    },
+    
+    { 
+        question: "This Philp Pullman series is often viewed as an atheist counterpoint to the world of Narnia",
+        choices: ["His Dark Materials", "American Gods", "Neverwhere", "Assassin's Apprentice"],
+        correctChoice: "His Dark Materials"
+    
+    },
+    
+    { 
+        question: "What is the name of the protagonist wizard in 'The Lord of the Rings",
+        choices: ["Merlin" , "Dumbledore", "Oz", "Gandalf"],
+        correctChoice: "Gandalf"
+    
+    }, 
+    
+    { 
+        question: "Who brought the lamp post into Narnia? The Magician's Nephew",
+        choices: ["Jadis" , "Aslan", "Mr. Tumnus", "Polly"],
+        correctChoice: "Jadis"
+    
+    }
+    
+    
+    ];
+    $(document).ready(function() {
 
-},
+function displayQuestion() {
+    
+    //display the question and choices
 
-{
-    question: "In what book do 24 children fight to the death?" ,
-    choices: ["Harry Potter", "Hunger Games", "Eragon", "Lord of the Rings"],
-    correctChoice: "Hunger Games"
-},
+var question = questions[currentQuestion].question;
+var choices = questions[currentQuestion].choices; 
 
-{ 
-    question: "What is the name of the lion resurrected in C.S. Lewis' tales of Narnia?",
-    choices: ["Adam" , "Jesus", "Aslan", "James"],
-    correctChoice: "Aslan"
-
-},
-
-{ 
-    question: "This Philp Pullman series is often viewed as an atheist counterpoint to the world of Narnia",
-    choices: ["His Dark Materials", "American Gods", "Neverwhere", "Assassin's Apprentice"],
-    correctChoice: "His Dark Materials"
-
-},
-
-{ 
-    question: "What is the name of the protagonist wizard in 'The Lord of the Rings",
-    choices: ["Merlin" , "Dumbledore", "Oz", "Gandalf"],
-    correctChoice: "Gandalf"
-
-}, 
-
-{ 
-    question: "Who brought the lamp post into Narnia? The Magician's Nephew",
-    choices: ["Jadis" , "Aslan", "Mr. Tumnus", "Polly"],
-    correctChoice: "Jadis"
-
+    $('#quizDiv').html('<h4>'+ question +'</h4>');
+    $('#timer').html('Timer: ' + counter);
 }
 
+displayQuestion(); 
+console.log("is it working?")
+console.log ( $ );
 
-];
-
+});
