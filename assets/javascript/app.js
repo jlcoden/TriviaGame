8a88975ba15d679508669b5bb243e3a8ce2e407c
Josh Cosson <joshlcosson@gmail.com>
2019-09-25 23:26:49 -0700

    added additional comments to javascript code also cleaned up code on app.js, style.css, and index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,29 @@
-//press start to start the game
-//timer starts count down from 30 seconds
-//select answer, it will say if it is correct or not
-//if run out of time, state that they ran out of time and move on to the next question 
-//At the end show the correct and incorrect answers, timer is no longer clicking down. 
-//Win clicking on 'Start Over' it resets the game 
+//counter for counting down on questions
 let counter = 20;
-var correctChoice = 0; 
+//veriable to setting correctChoice
+var correctChoice; 
+//var for keeping track of losses
 var losses = 0; 
+//var for keeping track of wins
 var wins = 0; 
-var currentQuestion = 0;  
-var choices = 0; 
+//var for keeping track of current question
+var currentQuestion = 0; 
+// variable for keeping track of choices 
+var choices; 
+//variable for setting intervalId for timer
 var intervalId; 
-var choiceOptions; 
+//counter for how long to show gifs
 var imageCounter = 6; 
+//imageInterval for image timer
 var imageIntervalId = 0; 
+// variable to keep track of unanswered questions
 var unanswered = 0; 
 
 
-
+//questions object
 var questions = [
 
-    {
+    {   //question 1
         question:"This George R.R Martin series of books was adapted into HBO's popular 'Game of Thrones' TV series",
         choices: ["The Chronicals of Narnia", "A song of Ice and fire", "The Rain Wilds Chronicals", "Iron Swords Trilogy"],
         correctChoice: "A song of Ice and fire",
@@ -29,7 +32,7 @@ var questions = [
      
     },
     
-    {
+    {   //question 2
         question: "In what book do 24 children fight to the death?" ,
         choices: ["Harry Potter", "Hunger Games", "Eragon", "Lord of the Rings"],
         correctChoice: "Hunger Games",
@@ -37,7 +40,7 @@ var questions = [
         incorrectImage:"hgIncorrect.gif"
     },
     
-    { 
+    {   //question 3
         question: "What is the name of the lion resurrected in C.S. Lewis' tales of Narnia?",
         choices: ["Adam" , "Jesus", "Aslan", "James"],
         correctChoice: "Aslan",
@@ -46,7 +49,7 @@ var questions = [
     
     },
     
-    { 
+    {   //question 4
         question: "This Philp Pullman series is often viewed as an atheist counterpoint to C.S Lewis's world of Narnia",
         choices: ["His Dark Materials", "American Gods", "Neverwhere", "Assassin's Apprentice"],
         correctChoice: "His Dark Materials",
@@ -56,7 +59,7 @@ var questions = [
     },
 
 
-    { 
+    {   //question 5
         question: "What is the name of the protagonist wizard in 'The Lord of the Rings'?",
         choices: ["Merlin" , "Dumbledore", "Oz", "Gandalf"],
         correctChoice: "Gandalf",
@@ -71,182 +74,248 @@ var questions = [
 
 
     
-
+    //document wrapper 
     $(document).ready(function() {
 
-
-function showScore() {
-    console.log("show score");
-    clearInterval(intervalId);
-    
+    //function to showScore at end of the game
+    function showScore() {
+        //console log for debugging 
+        console.log("show score");
+        //clear intervalId
+        clearInterval(intervalId);
+    //empty the container div and create/append paragraphs for showing the score
     $('.container').empty().append(
+        //add paragraph to show number of correct answers
         $('<p/>')
           .addClass("score")
           .text('Correct Answers: ' + wins)
       );
       $('.container').append(
+        //add paragraph to show number of incorrect answers
         $('<p/>')
           .addClass("score")
           .text('Incorrect Answers: ' + losses)
       );
 
       $('.container').append(
+        //add paragraph to show number of unaswered questions
         $('<p/>')
           .addClass("score")
           .text('Unanswered: ' + unanswered)
       );
   
-  
+        //create and add Reset Button for resetting the page
         var r= $('<input type="button" class = "resetBtn" value="Reset" onClick="window.location.reload();"/>');
         $(".container").append(r);
 
 }
 
 
+//function for loading next question 
 function nextQuestion() {
+    //console log for debugging
     console.log("Next question");
+    //set counter to 20
     counter = 20; 
-
+    //add h2 tag to show timer 
     $("h2").show("#timer");
+    //check to see if on last question 
     var lastQuestion = (questions.length - 1) === currentQuestion; 
-
+    //if last question, go to showScore function 
     if (lastQuestion) {
+        
         showScore(); 
-
+    
     } else {
-
+    //else incriment currentQuestion counter 
     currentQuestion++; 
+    //empty quizResults div of previous question, and display Question
     $('#quizResults').empty();
     displayQuestion(); 
     }
 }
 
-function loadImage(status) {
-    console.log("Load Image"); 
-imageCounter = 6; 
-imageCounter--; 
 
+//function to load and display gif for correct or incorrect answer
+function loadImage(status) {
+//console log for debugging 
+console.log("Load Image"); 
+//set image counter to 6 seconds, showing the gif for 6 seconds
+imageCounter = 6; 
+//decrement the image counter
+imageCounter--; 
+//if the win status is brought over from player choice
  if (status === 'win')  {
+     //increment number of wins 
      wins++; 
- $("h2").hide("#timer");
+     //hide timer
+    $("h2").hide("#timer");
+    //empty the quizQuestions div and display the gif for the correct answer 
     $('#quizQuestions').empty().append('<img src= assets/images/'+ image +'>');
+    //empty the quizResults div and add paragraphs stating the player chose the correct answer
     $('#quizResults').empty().append(
         $('<p/>')
           .addClass("correctAnswer")
           .text("Congrats, that's correct!")
       );
+      //set Timeout for the nextQuestion method to 30 seconds 
       setTimeout(nextQuestion, 3 * 1000);
+//else if it's not the win status and the wrong answer is chosen
  }else{
+     //increment losses 
      losses++; 
+     //hide timer 
     $("h2").hide("#timer");
+    //empty quizQuestions div and display images for incorrect answer 
     $('#quizQuestions').empty().append('<img src= assets/images/'+ badImage +'>');
+    //empty quizResults and add paragraph to state they chose the incorrect answer 
     $('#quizResults').empty().append(
         $('<p/>')
           .addClass("correctAnswer")
           .text("Sorry, that's incorrect!")
           );
-      
-          $('#quizResults').append( $('<p/>') 
-          //show correct answer 
+          //add additional paragraph to state what the correct choice was 
+          $('#quizResults').append( $('<p/>')  
           .addClass("correctAnswer")
+          //show correctChoice
           .text("The correct answer was " + correctChoice )
           );
+      //setTimeOut for nextQuestion method for 30 seconds 
       setTimeout(nextQuestion, 3 * 1000);
 }
-if (counter === 0) {
+//after loading image and the timer hits 0 go to imageTimerStop function
+if (imageCounter === 0) {
+    //imageTimerStop
+    imageTimerStop(); 
 
-    imageStop(); 
-
+    }
 }
-}
 
-function imageStop() {
-    console.log("imageStop"); 
+//function for stopping image timer 
+function imageTimerStop() {
+    //console log for debugging 
+    console.log("imageTimerStop"); 
+    //clearInterval for IMageIntervalId
     clearInterval(imageIntervalId); 
+    //go to and set timeout for nextQuestion fuction, 30 seconds
     setTimeout(nextQuestion, 3 * 1000);
 }
 
-
-function stop() {
-    console.log("stop"); 
+//function to detect timer at 0 and tracking unanswered questions
+function timerStop() {
+    //console log for debugging 
+    console.log("timerStop"); 
+        //clear IntervalId
         clearInterval(intervalId);
+        //increment unanswered counter
         unanswered++; 
+        //navigate to and setTimeout for nextQuestion function 
         setTimeout(nextQuestion, 3 * 1000);
     }
 
-
+//function to count down timer for questions
 function decrement() {
+    //console log for debugging 
     console.log("decrement");
+    //decrement counter
     counter--; 
+    //display timer in timer div 
     $('#timer').html('Timer: ' + counter); 
 
+    //if counter goes to zero go to timerStop function 
     if (counter === 0) {
 
-        stop(); 
+        timerStop(); 
 
     }
 
 }
 
-    
+//displayQuestion Method
 function displayQuestion() {
+    //set counter time
     counter = 20; 
+    //set Interval for counting down 
     intervalId = setInterval(decrement, 1000);
-    console.log("Load Question"); 
-  $("h2").show("#timer");
-     $("a").remove(".btn");
+    //set choices to 0
     choices = 0; 
-    //display the question and choices
+    //console log for debugging 
+    console.log("Load Question"); 
+    //create an h2 tag to show timer 
+    $("h2").show("#timer");
+    //remove the start button 
+    $("a").remove(".btn");
+   // var for displaying question of current question 
     var question = questions[currentQuestion].question;
+    //var for displaying choices of current question 
     var choices = questions[currentQuestion].choices; 
+    //var for displaying correct Image of current question 
     var correctImage = questions[currentQuestion].correctImage;
+    //var for displaying incorrect Image of current question 
     var incorrectImage = questions[currentQuestion].incorrectImage;
 
+    //create h4 tag and place in quizQuestions div for holding the question 
     $('#quizQuestions').html('<h4 class = "listQuestion">'+ question +'</h4>');
+    //show count down time in timer div
     $('#timer').html('Timer: ' + counter);
+    //go to displayChoices method to show the choices
     displayChoices(choices); 
     
 }
 
+//function display choices 
 function displayChoices(choices) {
+    // console log for debugging 
     console.log("Display Choices"); 
-for (var i = 0; i < choices.length; i++){
-
-questionOptions = $('#quizResults').append(
-    $('<p/>')
-      .attr("id", choices[i])
-      .addClass("choice")
-      .text(choices[i])
-  );
+    // for loop to display all choices for question 
+    for (var i = 0; i < choices.length; i++){
+        //append questions to the quizResults div as paragraphs
+        questionOptions = $('#quizResults').append(
+        $('<p/>')
+        .attr("id", choices[i])
+        .addClass("choice")
+        .text(choices[i])
+    );
 
    }
-
 }
 
+//On click function for detecting player clicking on Start
 $(".btn").on('click', function() {
+    //set variables to zero 
     currentQuestion = 0;
     wins = 0; 
     losses = 0; 
     intervalId = null;
+    //console log for debugging
     console.log("On click start"); 
+    //go to displayQuestion method to display the question 
     displayQuestion(); 
 
 });
 
 
+//On click function for detecting Player choice selection
 $(document).on("click", ".choice", function(){
-    clearInterval(intervalId);
+    //Upon click clear timer
+    clearInterval(intervalId); 
+    //console log for debugging
     console.log("On click select choice"); 
+    //detect player choice when clicking on the paragraph of choice options
     selectedChoice = ($(this).attr('id'));
+    //create variable for correct choice of current question
     correctChoice = questions[currentQuestion].correctChoice;
+    //create variable for the correct image of the current question 
     image = questions[currentQuestion].correctImage;
+    //create variable for the bad image of the current question 
     badImage = questions[currentQuestion].incorrectImage;
 
+    //if the player selected choice is the correct go to loadImage method with the 'win' status
     if (correctChoice === selectedChoice) {
         loadImage('win');
-
+//else go to loadImage method with 'loss' status
     } else { 
-        loadImage('lost'); 
+        loadImage('loss'); 
 
     }
 
